55e5159176569c2f7c4dcbca35612e986f8f7921
Samantha Carrillo <samanthacarrillo@Samanthas-MacBook-Pro.local>
2018-01-27 14:30:16 -0500

    routes for new links

--- hack-frontend/routes/index.js
+++ hack-frontend/routes/index.js
@@ -3,9 +3,32 @@ const router = express.Router();
 const request = require('request');
 const config = require('../app/models/config');
 
+
+/*=============================================
+=            Routes for users
+=============================================*/
+
 router.get('/', (req, res, next) => {
     return res.render('index');
 });
+
+router.get('/register', (req, res, next) => {
+    return res.render('register');
+});
+
+router.get('/login', (req, res, next) => {
+    return res.render('login');
+});
+
+router.get('/test', (req, res, next) => {
+    return res.render('test');
+});
+
+router.get('/soundtrack', (req, res, next) => {
+    return res.render('soundtrack');
+});
+
+
 
 router.post('/register', (req, res, next) => {
   request.post({
