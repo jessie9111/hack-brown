3cb71c9b3e5687a3756f5785d1d02c907cd356f5
Samantha Carrillo <samanthacarrillo@Samanthas-MacBook-Pro.local>
2018-01-28 10:16:19 -0500

    style too

--- hack-frontend/routes/index.js
+++ hack-frontend/routes/index.js
@@ -23,7 +23,6 @@ router.post('/register', (req, res, next) => {
   }).pipe(res)
 })
 
-
 router.get('/login', (req, res, next) => {
     return res.render('login');
 });
@@ -35,12 +34,75 @@ router.post('/login', (req, res, next) => {
   }).pipe(res)
 });
 
-router.get('/test', (req, res, next) => {
-    return res.render('test');
+/*=============================================
+=            Playlist Routes
+=============================================*/
+router.get('/INTJ', (req, res, next) => {
+    return res.render('INTJ');
 });
 
-router.get('/soundtrack', (req, res, next) => {
-    return res.render('soundtrack');
+router.get('/ESFJ', (req, res, next) => {
+    return res.render('ESFJ');
+});
+
+router.get('/INTP', (req, res, next) => {
+    return res.render('INTP');
+});
+
+router.get('/ENTP', (req, res, next) => {
+    return res.render('ENTP');
+});
+
+router.get('/ENTJ', (req, res, next) => {
+    return res.render('ENTJ');
+});
+
+router.get('/INFJ', (req, res, next) => {
+    return res.render('INFJ');
+});
+
+router.get('/INFP', (req, res, next) => {
+    return res.render('INFP');
+});
+
+router.get('/ENFJ', (req, res, next) => {
+    return res.render('ENFJ');
+});
+
+router.get('/ENTP', (req, res, next) => {
+    return res.render('ENTP');
+});
+
+router.get('/ENFP', (req, res, next) => {
+    return res.render('ENFP');
+});
+
+router.get('/ISTJ', (req, res, next) => {
+    return res.render('ISTJ');
+});
+
+router.get('/ISFJ', (req, res, next) => {
+    return res.render('ISFJ');
+});
+
+router.get('/ESTJ', (req, res, next) => {
+    return res.render('ESTJ');
+});
+
+router.get('/ISTP', (req, res, next) => {
+    return res.render('ISTP');
+});
+
+router.get('/ISFP', (req, res, next) => {
+    return res.render('ISFP');
+});
+
+router.get('/ESTP', (req, res, next) => {
+    return res.render('ESTP');
+});
+
+router.get('/ESFP', (req, res, next) => {
+    return res.render('ESFP');
 });
 
 module.exports = router;
